2d95d10789ffc41fd51ca895964bd6424381bcf6
yamilaBacchini <y.bacchini@hotmail.com>
2020-05-04 22:26:33 -0300

    Add AdoptionCat-Cards and display on Home Results

--- src/components/Home/Home-Results.js
+++ src/components/Home/Home-Results.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import AdoptionCatCard from '../AdoptionCat/AdoptionCar-Card'
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
 
 class HomeResults extends React.Component {
     render() {
+        let nums = Array.from(Array(9).keys());
         return (
             <React.Fragment>
-                <Container fixed>
-                    <Typography component="div" style={{ backgroundColor: '#eee6f0', height: '100vh' }} />
+                <Container fixed style={{backgroundColor:'#eee6f0', minHeight: '100vh', minWidth:'90vh', padding: 30 }}>
+                    <GridList cols={3}>
+                        {nums.map(n => {
+                            return (
+                                <GridListTile key={n} style={{height: 500}}>
+                                    <AdoptionCatCard />
+                                </GridListTile>
+                            );
+                        })}
+                    </GridList>
                 </Container>
             </React.Fragment>
         );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home/Home';
-import MenuToolBar from "./components/shared/MenuToolBar";
+import MenuToolBar from "./components/Shared/MenuToolBar";
 
 ReactDOM.render(<MenuToolBar />, document.getElementById('menuToolBar'));
 
